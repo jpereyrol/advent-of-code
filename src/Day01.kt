a625eae96e055b030e65e6c44fb6f6c3ccb2d3a2
jpereyrol <jpereyrol@gmail.com>
2022-12-01 19:09:39 -0500

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val caloriesPerElf = input.getCaloriesPerElfAsList()
+        return caloriesPerElf.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val caloriesPerElf = input.getCaloriesPerElfAsList()
+        return caloriesPerElf
+            .asSequence()
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun List<String>.getCaloriesPerElfAsList(): List<Int> {
+    var currentSum = 0
+    val caloriesPerElf: MutableList<Int> = mutableListOf()
+
+    for (line in this) {
+        if (line == "") {
+            caloriesPerElf.add(currentSum)
+            currentSum = 0
+        } else {
+            currentSum += line.toInt()
+        }
+    }
+    caloriesPerElf.add(currentSum)
+
+    return caloriesPerElf.toList()
 }
