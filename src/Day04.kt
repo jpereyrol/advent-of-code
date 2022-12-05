86631aa8df4f718e81caf5ac3575e418c10cc4e7
jpereyrol <jpereyrol@gmail.com>
2022-12-04 21:11:24 -0500

    day 4

---
+++ src/Day04.kt
@@ -0,0 +1,50 @@
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val firstSplit = input.map { it.split(",") }
+
+        val secondSplit = firstSplit.map { it.map { it.split("-") } }.map { it.map { it.map { it.toInt() } } }
+
+        val ranges = secondSplit.map { it.map { IntRange(it.first(), it.last()) } }
+
+        return ranges.sumOf { it.first().overlapsFully(it.last()).toInt() }
+    }
+
+    fun part2(input: List<String>): Int {
+        val firstSplit = input.map { it.split(",") }
+
+        val secondSplit = firstSplit.map { it.map { it.split("-") } }.map { it.map { it.map { it.toInt() } } }
+
+        val ranges = secondSplit.map { it.map { IntRange(it.first(), it.last()) } }
+
+        return ranges.sumOf { it.first().overlaps(it.last()).toInt() }
+    }
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+
+}
+
+fun Boolean.toInt(): Int {
+    return if (this) 1 else 0
+}
+
+fun IntRange.overlapsFully(other: IntRange): Boolean {
+    val resultRange = IntRange(max(this.first, other.first), min(this.last, other.last))
+
+    return if (resultRange.first <= resultRange.last) {
+        //overlap
+        resultRange == this || resultRange == other
+    } else {
+        //no overlap
+        false
+    }
+}
+
+fun IntRange.overlaps(other: IntRange): Boolean {
+    return this.intersect(other).isNotEmpty()
+}
